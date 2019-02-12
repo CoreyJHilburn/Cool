e04abcbd2fb4436a04e7f581381c4ec1b5c8dcf5
corey hall <coreyhall@verizon.net>
2019-02-11 22:06:48 -0600

    youre not cool

--- CoreyHallClub/Controllers/HomeController.cs
+++ CoreyHallClub/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CoreyHallClub.Models;
+using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,18 +16,18 @@ namespace CoreyHallClub.Controllers
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public JsonResult Cool(Person P, string CoolName)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            using (var C = new SqlConnection("Server=.\\SQLExpress;Database=coreyhallclub;Trusted_Connection=True;"))
+            {
+                if (CoolName != "")
+                {
+                    C.Execute(@"INSERT INTO[dbo].[Person]([Name])VALUES(@CoolName)", new { CoolName });
+                    return Json(new { cool = true });
+                }               
+                return Json(new { cool = C.Query<Person>("SELECT * FROM person WHERE name = @Name", new { P.Name }).Any() });
+            }
         }
     }
 }
---
+++ CoreyHallClub/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoreyHallClub.Models
+{
+    public class Person
+    {
+        public Guid PersonUid { get; set; }
+        public string Name { get; set; }
+    }
+}
