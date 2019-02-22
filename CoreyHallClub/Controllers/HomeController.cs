5f09623c901a7e7140496de950ed0856a12f7847
corey hall <coreyhall@verizon.net>
2019-02-21 23:04:36 -0600

    azure

--- CoreyHallClub/Controllers/HomeController.cs
+++ CoreyHallClub/Controllers/HomeController.cs
@@ -19,11 +19,11 @@ namespace CoreyHallClub.Controllers
         [HttpPost]
         public JsonResult Cool(Person P, string CoolName)
         {
-            using (var C = new SqlConnection("Server=.\\SQLExpress;Database=coreyhallclub;Trusted_Connection=True;"))
+            using (var C = new SqlConnection("Server=tcp:coreyhallclubdbserver.database.windows.net;Initial Catalog=CoreyHallClub_db;Persist Security Info=False;User ID=coreyhall27;Password=hC174377@#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
             {
                 if (CoolName != "")
                 {
-                    C.Execute(@"INSERT INTO[dbo].[Person]([Name])VALUES(@CoolName)", new { CoolName });
+                    C.Execute(@"INSERT INTO[dbo].[Person]([Name])VALUES(@CoolName)", new { CoolName = CoolName.ToUpper() });
                     return Json(new { cool = true });
                 }               
                 return Json(new { cool = C.Query<Person>("SELECT * FROM person WHERE name = @Name", new { P.Name }).Any() });
